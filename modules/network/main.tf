f937ee582c318a035a135a75e7e03da7b752c54c
Doug Morand <dmorand@gmail.com>
2026-06-01 22:44:58 -0400

    refactor: Split route table routes into standalone aws_route resources

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -59,32 +59,36 @@ resource "aws_nat_gateway" "main" {
   })
 }
 
-# Public Route Table
+# Public Route Table (routes managed as standalone aws_route resources so
+# callers can add their own routes without conflicting with this module)
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
-  }
 
   tags = merge(var.tags, {
     Name = "${var.project_name}-public-rt"
   })
 }
 
-# Private Route Table
+resource "aws_route" "public_default" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+}
+
+# Private Route Table (routes managed as standalone aws_route resources so
+# callers can add their own routes without conflicting with this module)
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.main.id
-  }
 
   tags = merge(var.tags, {
     Name = "${var.project_name}-private-rt"
   })
+}
+
+resource "aws_route" "private_default" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.main.id
 }
 
 # Route Table Associations
